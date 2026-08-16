df07aa58f0d99eee7ff346eef7adce6213619e26
Anfal-Jamil <infaaljamil@gmail.com>
2026-08-16 13:16:47 +0500

    fix start script for railway

--- server/server.js
+++ server/server.js
@@ -34,5 +34,5 @@ app.post("/api/scan", async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
